08c898bc06b69935fee6d971db155a7a658a6b7c
AbonyiXavier <francisabonyi@gmail.com>
2020-03-25 22:48:03 +0100

    product detail design

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-color mt-3">
+  <div class="bg-color mt-3 pb-3">
     <div class="wrapper">
       <b-container>
         <b-row class="text-center">
@@ -7,12 +7,12 @@
             <div class="footer-item">
               <h6 class="mt-3">LET US HELP YOU</h6>
               <ul>
-                <a href>
-                  <li>Help Center</li>
-                </a>
-                <a href>
-                  <li>Contact Us</li>
-                </a>
+                <li>
+                  <a href>Help Center</a>
+                </li>
+                <li>
+                  <a href>Contact Us</a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -23,15 +23,15 @@
                 <i class="fas fa-shopping-cart"></i>
               </h6>
               <ul>
-                <a href>
-                  <li>About Us</li>
-                </a>
-                <a href>
-                  <li>Terms & Condition</li>
-                </a>
-                <a href>
-                  <li>Privacy policy</li>
-                </a>
+                <li>
+                  <a href>About Us</a>
+                </li>
+                <li>
+                  <a href>Terms & Condition</a>
+                </li>
+                <li>
+                  <a href>Privacy policy</a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -39,12 +39,12 @@
             <div class="footer-item">
               <h6 class="mt-3">EARN WITH US</h6>
               <ul>
-                <a href>
-                  <li>Become a Sales Consultant</li>
-                </a>
-                <a href>
-                  <li>An Affliated Partner</li>
-                </a>
+                <li>
+                  <a href>Become a Sales Consultant</a>
+                </li>
+                <li>
+                  <a href>An Affliated Partner</a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -52,18 +52,18 @@
             <div class="footer-item">
               <h6 class="mt-3">COUNTRIES</h6>
               <ul>
-                <a href>
-                  <li>Nigeria</li>
-                </a>
-                <a href>
-                  <li>Kenya</li>
-                </a>
-                <a href>
-                  <li>South Africa</li>
-                </a>
-                <a href>
-                  <li>Ghana</li>
-                </a>
+                <li>
+                  <a href>Nigeria</a>
+                </li>
+                <li>
+                  <a href>Kenya</a>
+                </li>
+                <li>
+                  <a href>South Africa</a>
+                </li>
+                <li>
+                  <a href>Ghana</a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -76,26 +76,27 @@
             <div class="footer-item mt-2">
               <h6>Follow us on</h6>
               <ul class="ul">
-                <a href="https://facebook.com/francis.abonyi" target="_blank">
-                  <li>
+                <li>
+                  <a href="https://facebook.com/francis.abonyi" target="_blank">
                     <i class="fab fa-facebook-f"></i>
-                  </li>
-                </a>
-                <a href="https://twitter.com/abonyi_Xavier" target="_blank">
-                  <li>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/abonyi_Xavier" target="_blank">
                     <i class="fab fa-twitter"></i>
-                  </li>
-                </a>
-                <a href="https://instagram.com/Xavierblaze_art" target="_blank">
-                  <li>
+                  </a>
+                </li>
+
+                <li>
+                  <a href="https://instagram.com/Xavierblaze_art" target="_blank">
                     <i class="fab fa-instagram"></i>
-                  </li>
-                </a>
-                <a href>
-                  <li>
+                  </a>
+                </li>
+                <li>
+                  <a href>
                     <i class="fab fa-youtube"></i>
-                  </li>
-                </a>
+                  </a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -103,19 +104,21 @@
             <div class="footer-item mt-2">
               <h6>Methods of payment and delivery partner</h6>
               <ul class="ul">
-                <a href>
-                  <li>
+                <li>
+                  <a href>
                     <i class="fab fa-cc-visa"></i> Visa
-                  </li>
-                </a>
-                <a href>
-                  <li>Verve</li>
-                </a>
-                <a href>
-                  <li>
+                  </a>
+                </li>
+
+                <li>
+                  <a href>Verve</a>
+                </li>
+
+                <li>
+                  <a href>
                     <i class="fab fa-dhl"></i>
-                  </li>
-                </a>
+                  </a>
+                </li>
               </ul>
             </div>
           </b-col>
@@ -150,25 +153,25 @@
     </div>
     <div class="footer-item text-center hide-icon">
       <ul class="ul">
-        <a href="https://facebook.com/francis.abonyi" target="_blank">
-          <li>
+        <li>
+          <a href="https://facebook.com/francis.abonyi" target="_blank">
             <i class="fab fa-facebook-f"></i>
-          </li>
-        </a>
-        <a href="https://twitter.com/abonyi_Xavier" target="_blank">
-          <li>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/abonyi_Xavier" target="_blank">
             <i class="fab fa-twitter"></i>
-          </li>
-        </a>
-        <a href="https://instagram.com/Xavierblaze_art" target="_blank">
-          <li>
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/Xavierblaze_art" target="_blank">
             <i class="fab fa-instagram"></i>
-          </li>
-        </a>
+          </a>
+        </li>
       </ul>
     </div>
     <div>
-      <h6 class="text-center copyright pt-1">
+      <h6 class="text-center copyright pt-1 mb-0">
         Xavier's
         <i class="fas fa-shopping-cart"></i> &copy; Copyright 2020
       </h6>
@@ -186,23 +189,26 @@ export default {
 .bg-color {
   /* background-color: black; */
   background-color: rgb(32, 32, 32);
-  height: 50vh;
+  /* height: 46vh; */
 }
 .footer-item {
   color: #fff;
   font-size: 14px;
+  text-align: left;
+}
+ul {
+  padding: 0;
 }
 ul li {
-  text-decoration: none;
   list-style: none;
+}
+ul li a {
+  text-decoration: none;
   font-size: 14px;
   color: #fff;
 }
-ul a li {
-  text-decoration: none;
-}
+
 .ul li {
-  list-style: none;
   display: inline-block;
   padding: 9px;
   font-size: 14px;
@@ -237,7 +243,7 @@ h6 {
   .bg-color {
     /* background-color: black; */
     background-color: rgb(32, 32, 32);
-    height: 80vh;
+    /* height: 60vh; */
   }
   .footer-item {
     color: #fff;
@@ -298,7 +304,7 @@ h6 {
   }
   .bg-color {
     background-color: rgb(32, 32, 32);
-    height: 25vh;
+    /* height: 25vh; */
   }
   .copyright {
     font-size: 10px;
